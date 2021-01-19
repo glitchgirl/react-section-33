907db0d2336ae406ac7e96985b348d31d815e0c4
glitchgirl <londonspleen@gmail.com>
2021-01-19 15:21:04 -0500

    search bar

---
+++ weatherboy/src/containers/search_bar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input />
+        <span>
+          <button type="submit">search</button>
+        </span>
+      </form>
+    );
+  }
+}
